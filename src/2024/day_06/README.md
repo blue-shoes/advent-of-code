b39a215e522409bfc1bb4d338b374b0677a77714
blue-shoes <adam.m.scharf@gmail.com>
2024-12-07 16:24:24 -0600

    Python solution for Day 8, 2024, Part 2 (brute-force-ish)

---
+++ src/2024/day_06/README.md
@@ -0,0 +1,4 @@
+Day 6: Guard Gallivant: https://adventofcode.com/2024/day/6
+
+- Part 1: Find Guard Path steps
+- Part 2: Find obstacles to make a loop
--- src/2024/day_06/python/main.py
+++ src/2024/day_06/python/main.py
@@ -1,3 +1,4 @@
+import datetime
 import numpy as np
 from numpy import ndarray
 
@@ -8,7 +9,7 @@ DIRECTIONS: dict[str: tuple[str, tuple[int, int]]] = \
         'v': ('<', (0, 1)),
         '<': ('^', (-1, 0))
     }
-def main_part1(obstacles:ndarray, start_coordinate:tuple[int, int], start_dir:str):
+def main_part1(obstacles:ndarray, start_coordinate:tuple[int, int], start_dir:str, max_step:int=None) -> ndarray:
     visited_map = np.zeros(shape=obstacles.shape)
     direction = start_dir
     coord = start_coordinate
@@ -43,28 +44,38 @@ def main_part1(obstacles:ndarray, start_coordinate:tuple[int, int], start_dir:st
                 #column
                 if steps[1][1] > 0:
                     stop = obstacles.shape[0]
-                mark_visited_map(visited_map, start_1d, stop, steps[1][1], static, True)
+                mark_visited_map(visited_map, start_1d, stop, steps[1][1], static, True, max_step)
             else:
                 if steps[1][0] > 0:
                     stop = obstacles.shape[1]
-                mark_visited_map(visited_map, start_1d, stop, steps[1][0], static, False)
+                mark_visited_map(visited_map, start_1d, stop, steps[1][0], static, False, max_step)
             break
         if steps[1][0] == 0:
-            mark_visited_map(visited_map, start_1d, stop, steps[1][1], static, True)
+            mark_visited_map(visited_map, start_1d, stop, steps[1][1], static, True, max_step)
             coord = (stop-(steps[1][1]), coord[1])
         else:
-            mark_visited_map(visited_map, start_1d, stop, steps[1][0], static, False)
+            mark_visited_map(visited_map, start_1d, stop, steps[1][0], static, False, max_step)
             coord = (coord[0], stop-(steps[1][0]))
         
         direction = steps[0]
-    print(f'Visited {np.sum(visited_map)} sites')
+        if max_step and np.sum(visited_map) > max_step:
+            return visited_map, True
+    if not max_step:
+        print(f'Visited {np.sum(visited_map)} sites')
+    return visited_map, False
 
-def mark_visited_map(visited_map:ndarray, start_1d, stop, step, static, col):
+def mark_visited_map(visited_map:ndarray, start_1d, stop, step, static, col, max_step):
     for idx in range(start_1d, stop, step):
         if col:
-            visited_map[idx, static] = 1
+            if max_step:
+                visited_map[idx, static] += 1
+            else:
+                visited_map[idx, static] = 1
         else:
-            visited_map[static, idx] = 1
+            if max_step:
+                visited_map[static, idx] += 1
+            else:
+                visited_map[static, idx] = 1
 
 if __name__ == "__main__":
     with open("../inputs.txt") as file:
@@ -81,5 +92,25 @@ if __name__ == "__main__":
                 start_coordinate = (y_idx, x_idx)
     
     np_map = np.array(site_map)
-    main_part1(np_map, start_coordinate, current_dir)
+    visited_map, _ = main_part1(np_map, start_coordinate, current_dir)
+    #print(visited_map)
+    max_spots = visited_map.shape[0]*visited_map.shape[1]
+    sum_obs = 0
+    s_time = datetime.datetime.now()
+    for row_idx in range(visited_map.shape[0]):
+        for col_idx in range(visited_map.shape[1]):
+            if visited_map[col_idx, row_idx] == 1:
+                new_obs = np_map.copy()
+                new_obs[col_idx, row_idx] = '#'
+                if np_map[col_idx, row_idx] == current_dir:
+                    continue
+                _, loop = main_part1(new_obs, start_coordinate, current_dir, max_spots)
+                if loop:
+                    sum_obs += 1
+    e_time = datetime.datetime.now()
+    print(e_time - s_time)
+    print(f'Obstacles that make a loop: {sum_obs}')
+
+
+
 
