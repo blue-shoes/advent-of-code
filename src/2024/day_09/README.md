8015c11706c2d79491561c23da344903d9e08412
blue-shoes <adam.m.scharf@gmail.com>
2024-12-09 19:32:54 -0600

    Python solution for Day 9, 2024 Part 2

---
+++ src/2024/day_09/README.md
@@ -0,0 +1,4 @@
+Day 9: Disk Fragmenter: https://adventofcode.com/2024/day/9
+
+- Part 1: Fill empty disk space with broken records
+- Part 2: Fill empty disk space with complete records
--- src/2024/day_09/python/main.py
+++ src/2024/day_09/python/main.py
@@ -45,6 +45,36 @@ def main_part1(np_array:ndarray, block_array:ndarray):
                 current_insert_idx += 1
                 used_rev_file_blocks += 1
 
+def main_part2(np_array:ndarray, block_array:ndarray):
+    mod_space = {}
+    
+    for f_idx, f_val in enumerate(np_array):
+        idx = len(np_array) - f_idx - 1
+        val = np_array[idx]
+        if idx % 2 == 0:
+            insert = 0
+            for idx_front, space_val in enumerate(np_array):
+                if idx_front >= idx:
+                    for i in range(val):
+                        block_array[insert + i] = int(idx/2)
+                    break
+                if idx_front % 2 == 0:
+                    insert += space_val
+                    continue
+                if val > space_val:
+                    insert += space_val
+                    continue
+                
+                if idx_front in mod_space and val > space_val - mod_space[idx_front]:
+                    insert += space_val
+                    continue
+                
+                for i in range(val):
+                    block_array[insert + mod_space.get(idx_front, 0)] = int(idx / 2)
+                    insert += 1
+                mod_space[idx_front] = val + mod_space.get(idx_front, 0)
+                break
+
 def calc_checksum(block_array:ndarray) -> int:
     sum = 0
     for idx, val in enumerate(block_array):
@@ -61,4 +91,10 @@ if __name__ == "__main__":
 
     main_part1(np_array, block_array)
     cksum = calc_checksum(block_array)            
+    print(f'New checksum is {cksum}')
+
+    block_array = np.zeros(np.sum(np_array))
+    main_part2(np_array, block_array)
+    #print(block_array)
+    cksum = calc_checksum(block_array)
     print(f'New checksum is {cksum}')
