fa03413b09684dd9a8c94c3349cb46985ed08244
blue-shoes <adam.m.scharf@gmail.com>
2025-12-01 09:30:20 -0600

    2025 Day 1 Part 2 solution.

--- src/2025/day_01/src/main.rs
+++ src/2025/day_01/src/main.rs
@@ -15,6 +15,11 @@ fn main() {
     let part1_count = part1_solution(&seq, dial);
     println!("The dial rested at zero after {part1_count} steps.");
 
+    let dial = dial_start;
+    let part2_count: i32 = part2_solution(&seq, dial);
+    println!("The dial passed zero {part2_count} times.");
+
+
 }
 
 fn part1_solution(seq: &Vec<&str>, mut dial: i32) -> i32 {
@@ -36,4 +41,44 @@ fn part1_solution(seq: &Vec<&str>, mut dial: i32) -> i32 {
     }
 
     part1_count
+}
+
+fn part2_solution(seq: &Vec<&str>, mut dial: i32) -> i32 {
+    let mut count = 0;
+
+    for step in seq {
+        let dir = &step[..1];
+        let clicks = &step[1..].parse::<i32>().expect("Should have parsed number of clicks");
+
+        count += clicks / 100;
+
+        let partial = clicks % 100;
+
+        let orig_dial = dial;
+
+        dial = match dir {
+            "L" => dial - partial,
+            "R" => dial + partial,
+            _ => dial,
+        };
+
+        dial = dial % 100;
+        if dial < 0 {
+            dial += 100;
+        }
+
+        if orig_dial == 0 {
+            continue;
+        }
+
+        if dial == 0 {
+            count += 1;
+        }else if dir == "L" && dial > orig_dial{
+            count += 1;
+        } else if dir == "R" && dial < orig_dial{
+            count +=1;
+        }
+
+    }
+    count
 }
